fd2be8dee854622b2f83bb3204e430cdf3e1932d
muratjumashev <jumasheff@gmail.com>
2019-03-15 11:42:14 +0600

    Add static settings

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
@@ -102,6 +102,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'media', 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media', 'shared_static'),
+]
 
 SITE_ID = 1
