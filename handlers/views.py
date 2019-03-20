4c2f4cac7e61d5dfc6124a1a5f290c4aebe057de
muratjumashev <jumasheff@gmail.com>
2019-03-20 18:53:03 +0600

    Add logging for debugging

--- handlers/views.py
+++ handlers/views.py
@@ -1,5 +1,14 @@
+import logging
 from telegrambot.bot_views.generic import TemplateCommandView
+
+logger = logging.getLogger(__name__)
 
 
 class StartView(TemplateCommandView):
     template_text = 'bot/messages/command_start_text.txt'
+
+    def handle(self, bot, update, **kwargs):
+        super().handle(bot, update, **kwargs)
+        chat_id = update.message.chat_id
+        log_msg = 'THIS IS IN StartView: {}'.format(chat_id)
+        logger.debug(log_msg)
