e52b67629b2b3d1294937b37e3f82eae204d67d5
muratjumashev <jumasheff@gmail.com>
2019-03-24 22:16:27 +0600

    Remove debug code

--- handlers/views.py
+++ handlers/views.py
@@ -6,10 +6,3 @@ logger = logging.getLogger(__name__)
 
 class StartView(TemplateCommandView):
     template_text = 'bot/messages/command_start_text.txt'
-    logger.debug('me here')
-
-    def handle(self, bot, update, **kwargs):
-        super().handle(bot, update, **kwargs)
-        chat_id = update.message.chat_id
-        log_msg = 'THIS IS IN StartView: {}'.format(chat_id)
-        logger.debug(log_msg)
