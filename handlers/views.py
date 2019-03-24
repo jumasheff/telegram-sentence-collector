8e4e39e0f03f97e9eea6bf1dad4ceddab8708a88
muratjumashev <jumasheff@gmail.com>
2019-03-24 23:30:06 +0600

    Add keyboard template

--- handlers/views.py
+++ handlers/views.py
@@ -6,3 +6,4 @@ logger = logging.getLogger(__name__)
 
 class StartView(TemplateCommandView):
     template_text = 'handlers/messages/command_start_text.txt'
+    template_keyboard = 'handlers/messages/command_languages_list_keyboard.txt'
