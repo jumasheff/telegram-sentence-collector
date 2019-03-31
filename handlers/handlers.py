977174b85f1e4f39a935b9536533c200b44f7b3a
muratjumashev <jumasheff@gmail.com>
2019-03-31 20:57:34 +0600

    Add AcceptView

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,8 +1,10 @@
 from telegrambot.handlers import command
 
-from handlers.views import start
+from handlers.views import StartView
+from handlers.views import AcceptView
 
 
 urlpatterns = [
-    command('start', start),
+    command('start', StartView.as_command_view()),
+    command('accept', AcceptView.as_command_view()),
 ]
--- handlers/views.py
+++ handlers/views.py
@@ -13,18 +13,7 @@ logger = logging.getLogger(__name__)
 
 class StartView(TemplateCommandView):
     template_text = 'handlers/messages/command_start_text.txt'
-    template_keyboard = 'handlers/messages/command_languages_list_keyboard.txt'
 
 
-def start(bot, update, **kwargs):
-    template_text = 'handlers/messages/command_start_text.txt'
-    text = TextResponse(template_text, {}).render()
-    keyboard = [[
-        InlineKeyboardButton('Yes', callback_data='0'),
-        InlineKeyboardButton('No', callback_data='1')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=text,
-        reply_markup=reply_markup,
-        parse_mode=ParseMode.MARKDOWN)
+class AcceptView(TemplateCommandView):
+    template_text = 'handlers/messages/command_accept_text.txt'
