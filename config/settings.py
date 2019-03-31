7b3cc069c3f16ec6ca4e4aaa927e96922c1f46a8
muratjumashev <jumasheff@gmail.com>
2019-03-31 22:47:49 +0600

    Add SAVE_UPDATES

--- config/settings.py
+++ config/settings.py
@@ -109,7 +109,8 @@ STATICFILES_DIRS = [
 ]
 
 TELEGRAM_BOT_HANDLERS_CONF = 'handlers.handlers'
-SITE_ID = 1
+SAVE_UPDATES = False
+/SITE_ID = 1
 
 DEBUG_LOG_FILE = os.path.join(BASE_DIR, 'debug.log')
 
