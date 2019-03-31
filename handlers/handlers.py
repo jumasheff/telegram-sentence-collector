71d6151c4bb68e80820be3010f12940529aac150
muratjumashev <jumasheff@gmail.com>
2019-03-31 22:45:17 +0600

    Tweak lang view

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -5,11 +5,13 @@ from handlers.views import StartView
 from handlers.views import AcceptView
 from handlers.views import ListLangsView
 from handlers.views import LangView
+from handlers.views import NewSentenceView
 
 
 urlpatterns = [
-    regex('^[a-z]{2}$', LangView.as_command_view()),
     command('start', StartView.as_command_view()),
     command('accept', AcceptView.as_command_view()),
     command('list', ListLangsView.as_command_view()),
+    regex(r'^[a-z]{2}$', LangView.as_command_view()),
+    command('new', NewSentenceView.as_command_view()),
 ]
--- handlers/views.py
+++ handlers/views.py
@@ -25,3 +25,17 @@ class ListLangsView(TemplateCommandView):
 
 class LangView(TemplateCommandView):
     template_text = 'handlers/messages/command_lang_text.txt'
+
+    def handle(self, bot, update, **kwargs):
+        ctx = self.get_context(bot, update, **kwargs)
+        text = TextResponse(self.template_text, ctx).render()
+        keyboard = [[
+            InlineKeyboardButton('New sentence', callback_data='0'),
+            InlineKeyboardButton('Validate sentences', callback_data='1')
+        ]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        bot.send_message(chat_id=update.message.chat_id, text=text, reply_markup=reply_markup)
+
+
+class NewSentenceView(TemplateCommandView):
+    template_text = 'handlers/messages/command_new_sentence_text.txt'
