de82413ca2cb027c0ec6bf5fbbbf0f670114fff6
muratjumashev <jumasheff@gmail.com>
2019-03-31 18:24:07 +0600

    Fix inline keyboard

--- handlers/views.py
+++ handlers/views.py
@@ -18,9 +18,14 @@ class StartView(TemplateCommandView):
 
 def start(bot, update, **kwargs):
     template_text = 'handlers/messages/command_start_text.txt'
-    template_keyboard = 'handlers/messages/command_languages_list_keyboard.txt'
     template_kbrd = get_template(template_keyboard)
     text = TextResponse(template_text, {}).render()
-    keyboard = [
-        [InlineKeyboardButton('Yes', callback_data='0'), InlineKeyboardButton('No', callback_data='1')]]
-    bot.send_message(chat_id=update.message.chat_id, text=text, reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
+    keyboard = [[
+        InlineKeyboardButton('Yes', callback_data='0'),
+        InlineKeyboardButton('No', callback_data='1')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=text,
+        reply_markup=reply_markup,
+        parse_mode=ParseMode.MARKDOWN)
