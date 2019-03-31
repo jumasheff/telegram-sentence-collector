c794406d93622adb35cbb1ff9d234f93ca06c18b
muratjumashev <jumasheff@gmail.com>
2019-03-31 18:17:47 +0600

    Use inline keybord

--- handlers/views.py
+++ handlers/views.py
@@ -1,5 +1,6 @@
 import logging
 from telegram import ParseMode
+from telegram import InlineKeyboardButton
 from telegram import InlineKeyboardMarkup
 from telegrambot.bot_views.generic import TemplateCommandView
 from telegrambot.bot_views.generic.responses import TextResponse
@@ -20,6 +21,6 @@ def start(bot, update, **kwargs):
     template_keyboard = 'handlers/messages/command_languages_list_keyboard.txt'
     template_kbrd = get_template(template_keyboard)
     text = TextResponse(template_text, {}).render()
-    keyboard = template_kbrd.render()
-    keyboard = InlineKeyboardMarkup(keyboard, resize_keyboard=True)
+    keyboard = [
+        [InlineKeyboardButton('Yes', callback_data='0'), InlineKeyboardButton('No', callback_data='1')]]
     bot.send_message(chat_id=update.message.chat_id, text=text, reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
